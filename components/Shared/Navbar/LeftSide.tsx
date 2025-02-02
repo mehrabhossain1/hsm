e892719a3c71c7b1a7c76045b03803e77b7b5991
mehrabhossain1 <mehrab.munna00@gmail.com>
2025-02-02 12:17:19 +0600

    left image alignment

--- components/Shared/Navbar/LeftSide.tsx
+++ components/Shared/Navbar/LeftSide.tsx
@@ -3,7 +3,7 @@ import SizearImage from "@/public/sizearImage.jpg";
 
 const LeftSide = () => {
   return (
-    <div className="flex gap-2 items-center justify-center pt-2 cursor-pointer">
+    <div className="flex gap-2 items-center justify-center py-1 cursor-pointer">
       <Image src={SizearImage} alt="sizear" className="rounded-full size-12" />
       <div className="text-white  text-xl">sizear</div>
     </div>
