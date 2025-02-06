c745926faa84bce7794d4bc92ae4b9eea47d88de
mehrabhossain1 <mehrab.munna00@gmail.com>
2025-02-06 23:06:57 +0600

    Stay Connected Form

--- components/Homepage/HeroSection/HeroSection.tsx
+++ components/Homepage/HeroSection/HeroSection.tsx
@@ -1,41 +1,20 @@
 "use client";
-import { ImagesSlider } from "@/components/ui/images-slider";
-import { motion } from "framer-motion";
-import React from "react";
+
+import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
 
 const HeroSection = () => {
-  const images = [
-    "https://images.unsplash.com/photo-1485433592409-9018e83a1f0d?q=80&w=1814&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1483982258113-b72862e6cff6?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1482189349482-3defd547e0e9?q=80&w=2848&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  ];
+  const words = `Knowledge for Solutions!
+  Don’t settle.
+  Better public health starts with knowledge.
+  HSM offers evidence and news within your reach.`;
 
   return (
-    <ImagesSlider images={images}>
-      <motion.div
-        initial={{
-          opacity: 0,
-          y: -80,
-        }}
-        animate={{
-          opacity: 1,
-          y: 0,
-        }}
-        transition={{
-          duration: 0.6,
-        }}
-        className="z-50 flex flex-col justify-center items-center text-center space-y-4"
-        style={{ minHeight: "calc(100vh - 80px)" }} // Adjusting for navbar height
-      >
-        <motion.p className="font-bold text-xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4 shadow-md">
-          The hero section slideshow <br /> nobody asked for
-        </motion.p>
-        <button className="px-6 py-3 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4 hover:bg-emerald-500/10">
-          <span>Join now →</span>
-          <div className="absolute inset-x-0 h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
-        </button>
-      </motion.div>
-    </ImagesSlider>
+    <div className="grid grid-cols-2 min-h-[calc(100vh-120px)] items-center px-4 bg-[#E0F2FF]">
+      <div>left</div>
+      <div>
+        <TextGenerateEffect duration={2} filter={false} words={words} />
+      </div>
+    </div>
   );
 };
 
