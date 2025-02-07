0ad701e4372d4807806cb58229db083224b4d610
mehrabhossain1 <mehrab.munna00@gmail.com>
2025-02-07 16:31:13 +0600

    Sticky Social  Icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import SocialShare from "@/components/Shared/Social/SocialShare";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <SocialShare />
         {children}
       </body>
     </html>
