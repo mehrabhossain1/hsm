cbcc279082d29922d546960064c9148b32dd727b
mehrabhossain1 <mehrab.munna00@gmail.com>
2025-02-12 19:58:07 +0600

    Resources page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import AboutSection from "@/components/Homepage/AboutSection/AboutSection";
+// import AboutSection from "@/components/Homepage/AboutSection/AboutSection";
 import BlogInterview from "@/components/Homepage/BlogInterview/BlogInterview";
 import Contact from "@/components/Homepage/Contact/Contact";
 // import { HeroScrollDemo } from "@/components/Homepage/HeroSection/HeroScrollDemo";
@@ -8,14 +8,18 @@ import NewsNewsletter from "@/components/Homepage/NewsNewsletter/NewsNewsletter"
 // import Sizear from "@/components/Homepage/Sizear/Sizear";
 // import { AnimatedTestimonialsDemo } from "@/components/Homepage/Testimonials/Testimonials";
 import Youtube from "@/components/Homepage/Youtube/Youtube";
+import AboutPage from "./about/page";
+import ResourcesAndBookmarksPage from "./resources-and-bookmarks/page";
 
 export default function Home() {
   return (
     <>
       {/* <HeroScrollDemo /> */}
       <HeroSection />
-      <AboutSection />
+      {/* <AboutSection /> */}
+      <AboutPage />
       {/* <ResourcesBookmarks /> */}
+      <ResourcesAndBookmarksPage />
       <NewsNewsletter />
       <BlogInterview />
       <Youtube />
