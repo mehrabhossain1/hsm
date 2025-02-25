56d46b81a36eab1f5867f9643ca14abd352211c2
Mehrab Hossain <mehrab.munna00@gmail.com>
2025-02-25 16:54:14 +0600

    add: Resources Page

--- app/bookmarks/page.tsx
+++ app/bookmarks/page.tsx
@@ -99,7 +99,7 @@ const BookmarksPage = () => {
       {/* Page Title Section */}
       <div className="relative w-full h-[150px] border-t-2 border-[#FFCE00]">
         <div className="absolute inset-0 bg-gradient-to-b from-[#2D8CBB]/90  px-8 to-[#2D8CBB]/30 flex items-center">
-          <div className="max-w-screen-xl mx-auto flex justify-between items-center w-full px-8 flex-wrap gap-6">
+          <div className="max-w-screen-xl mx-auto flex justify-between items-center w-full px-16 flex-wrap gap-6">
             <h1 className="text-5xl font-[1000] text-white">Bookmarks</h1>
             <p className="text-2xl max-w-3xl text-gray-800 font-semibold my-8 text-justify w-full md:w-auto">
               The Global Health Bookmarks section indicates the resources either
