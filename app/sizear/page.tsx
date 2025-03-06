3a0eac361160bd9e9a5aea30575261ec6156f16b
Mehrab Hossain <mehrab.munna00@gmail.com>
2025-03-06 19:54:04 +0600

    About sizear

--- app/sizear/page.tsx
+++ app/sizear/page.tsx
@@ -1,118 +1,23 @@
-import Image from "next/image";
+import AboutSizear from "@/components/Sizear/AboutSizear";
+import CareerTrajectory from "@/components/Sizear/CareerTrajectory";
+import MoreAboutMe from "@/components/Sizear/MoreAboutMe";
 
 const Sizear = () => {
   return (
     // min-h-[calc(100vh-115px)]
-    <section className="bg-gradient-to-b from-[#F9F6F3] to-white py-16 px-12">
+    // <section className="bg-gradient-to-b from-[#F9F6F3] to-white py-16 px-12">
+    <section className=" py-16 px-12">
       {/* Top section */}
-      <div className="max-w-screen-xl border mx-auto flex items-center px-4 sm:px-6 lg:px-8 w-full">
-        <div className="grid grid-cols-1 lg:grid-cols-[55%_45%] gap-4 lg:gap-6">
-          {/* Text Section - Left Side (55%) */}
-          <div className="space-y-6 order-2 lg:order-1 text-justify px-8">
-            <header className="space-y-2">
-              <h2 className="text-3xl md:text-3xl font-bold text-gray-800">
-                Monaemul Islam Sizear
-              </h2>
-              <p className="text-lg text-gray-600">
-                Founder of Health Systems Matter
-              </p>
-              <p className="text-md text-gray-500 italic">
-                Urban Public Health Anthropologist | Health System Activist
-              </p>
-            </header>
-
-            <div className="text-gray-700 leading-relaxed">
-              <p>
-                I’m Monaemul Islam Sizear, widely known as Sizear. Public health
-                is my profession and passion, pursued by choice. I currently
-                serve as a Technical Advisor for the{" "}
-                <a
-                  href="https://www.usaid.gov/global-health/health-areas/tuberculosis/resources/news-and-updates/global-accelerator-end-tb/health-systems-tb-hs4tb"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-blue-600 hover:underline font-medium"
-                >
-                  USAID Health System for TB project
-                </a>
-                , employed by{" "}
-                <a
-                  href="https://www.opendevelopment.co/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-blue-600 hover:underline font-medium"
-                >
-                  Open Development
-                </a>
-                . I also volunteer as the Organizing Secretary for the{" "}
-                <a
-                  href="https://publichealthfoundation.org.bd/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-blue-600 hover:underline font-medium"
-                >
-                  Public Health Foundation of Bangladesh
-                </a>
-                , one of the country’s largest civil society organizations.
-              </p>
-              <p className="mt-4">
-                My mission is to advance public health through evidence-based
-                approaches and stronger health systems. To support this, I
-                founded{" "}
-                <span className="font-semibold">Health Systems Matter</span>, a
-                platform leveraging a website and social media (
-                <a
-                  href="https://www.linkedin.com/company/health-systems-matter/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-blue-600 hover:underline"
-                >
-                  LinkedIn
-                </a>
-                ,{" "}
-                <a
-                  href="https://www.facebook.com/people/Health-Systems-Matter/61564171250656/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-blue-600 hover:underline"
-                >
-                  Facebook
-                </a>
-                ) to promote and disseminate critical public health issues. I
-                also write public health columns for national newspapers, blogs,
-                and have published research in peer-reviewed journals.
-                <span className="block mt-2 font-semibold italic">
-                  Sizear is pure health system.
-                </span>
-              </p>
-            </div>
-          </div>
-
-          {/* Image Section - Right Side (45%) */}
-          <div className="flex justify-center lg:justify-end order-1 lg:order-2">
-            <div className="relative w-64 h-64 md:w-80 md:h-80 lg:w-full lg:h-auto lg:max-w-full group">
-              <div className="absolute inset-0 bg-gradient-to-br from-blue-200 via-purple-200 to-pink-200 opacity-0 group-hover:opacity-50 transition-opacity duration-300"></div>
-
-              <Image
-                src="/sizearImage.webp"
-                alt="Monaemul Islam Sizear"
-                // fill
-                className="object-cover shadow-xl border-8 border-white transform z-10  hover:border-[#FFCE00]"
-                // sizes="(max-width: 768px) 256px, (max-width: 1024px) 320px, 384px"
-                priority
-                width={1000}
-                height={500}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
+      <AboutSizear />
       {/* Top section */}
 
       {/* More About Me */}
-      <div className="max-w-screen-xl mx-auto border sm:px-6 lg:px-16 py-16 ">
-        More About Me
-      </div>
+      <MoreAboutMe />
       {/* More About Me */}
+
+      {/* Career Trajectory / Employment Journey  */}
+      <CareerTrajectory />
+      {/* Career Trajectory / Employment Journey  */}
     </section>
   );
 };
---
+++ components/Sizear/CareerTrajectory.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { FaBriefcase } from "react-icons/fa";
+
+const CareerTrajectory = () => {
+  //   const data = [
+  //     {
+  //       title: "January 2023 – Present",
+  //       content: (
+  //         <div>
+  //           <p className="text-neutral-600  text-xl font-medium mb-8">
+  //             Technical Advisor, Open Development
+  //           </p>
+  //         </div>
+  //       ),
+  //     },
+  //     {
+  //       title: "November 2021 – December 2022 ",
+  //       content: (
+  //         <div>
+  //           <p className="text-neutral-600  text-xl font-medium mb-8">
+  //             Senior Program Analyst, ThinkWell Global
+  //           </p>
+  //         </div>
+  //       ),
+  //     },
+  //   ];
+
+  const experiences = [
+    {
+      title: "Technical Advisor",
+      company: "Open Development",
+      date: "January 2023 – Present",
+    },
+    {
+      title: "Senior Program Analyst",
+      company: "ThinkWell Global",
+      date: "November 2021 – December 2022",
+    },
+    {
+      title: "Program Officer",
+      company: "WaterAid",
+      date: "August 2018 – September 2020",
+    },
+    {
+      title: "Senior Program Coordinator",
+      company: "Sajida Foundation",
+      date: "February 2017 – July 2018",
+    },
+    {
+      title: "Project Coordinator",
+      company: "Sajida Foundation",
+      date: "March 2015 – January 2017",
+    },
+    {
+      title: "Research Officer",
+      company: "icddr,b",
+      date: "January 2015 – February 2015",
+    },
+    {
+      title: "District Coordinator",
+      company: "NGO Forum for Public Health",
+      date: "November 2012 – July 2013",
+    },
+    {
+      title: "Research Assistant",
+      company: "BARCIK",
+      date: "February 2011 – January 2012",
+    },
+  ];
+
+  return (
+    <div className="max-w-screen-xl mx-auto sm:px-6 lg:px-16 ">
+      <h2 className="text-2xl md:text-2xl font-bold text-gray-600  underline underline-offset-4">
+        Career Trajectory / Employment Journey
+      </h2>
+
+      <div className="relative max-w-3xl mx-auto pt-4">
+        {experiences.map((exp, index) => (
+          <motion.div
+            key={index}
+            initial={{ opacity: 0, x: -30 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5, delay: index * 0.2 }}
+            className="mb-6 relative"
+          >
+            <div className="absolute -left-[37px] top-2 bg-blue-500 w-12 h-12 flex items-center justify-center rounded-full">
+              <FaBriefcase className="text-white text-xl" />
+            </div>
+            <div className="bg-white p-8 shadow-lg rounded-lg">
+              <h3 className="text-xl font-semibold text-gray-900">
+                {exp.title}, {exp.company}
+              </h3>
+              {/* <p className="text-gray-700 text-xl">{exp.company}</p> */}
+              <p className=" text-gray-500 text-xl">{exp.date}</p>
+            </div>
+          </motion.div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CareerTrajectory;
