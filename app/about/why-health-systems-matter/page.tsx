e8caf7e14825573d0d1375fe67144065f73d9d92
Mehrab Hossain <mehrab.munna00@gmail.com>
2025-03-09 02:30:35 +0600

    why hsm ?

--- app/about/why-health-systems-matter/page.tsx
+++ app/about/why-health-systems-matter/page.tsx
@@ -9,9 +9,7 @@ const WhyHealthSystemsMatterPage = () => {
       <div className="relative w-full h-[150px] border-t-2 border-[#FFCE00] ">
         <div className="absolute inset-0 bg-gradient-to-b from-[#943AD8]/90 to-[#943AD8]/30 flex items-center px-16">
           <div className="max-w-screen-xl mx-auto flex justify-center items-center w-full px-8 flex-wrap gap-6">
-            <h1 className="text-5xl font-bold text-white">
-              Vision and Mission
-            </h1>
+            <h1 className="text-5xl font-bold text-white">Why HSM ?</h1>
             <p className="text-2xl max-w-3xl text-gray-800 font-semibold my-8 text-justify w-full md:w-auto"></p>
           </div>
         </div>
