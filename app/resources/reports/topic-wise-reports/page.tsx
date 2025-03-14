231dbed0eecf020a97e6434d24d4e7efe01ad141
Mehrab Hossain <mehrab.munna00@gmail.com>
2025-03-15 02:45:02 +0600

    health workers and human resources

--- app/resources/reports/topic-wise-reports/page.tsx
+++ app/resources/reports/topic-wise-reports/page.tsx
@@ -35,7 +35,7 @@ const bookmarksData = [
     },
     {
         title: "Health workers & human resources",
-        href: "/resources/reports/topic-wise-reports",
+        href: "/resources/reports/topic-wise-reports/health-workers-and-human-resources",
         description: "",
         Icon: "https://img.freepik.com/free-photo/business-leader-trader-searching-new-investment-solution_482257-116895.jpg?t=st=1741540814~exp=1741544414~hmac=00e99ff43576506148314d093bc85b7a592b6e0b4b18d00a2a9aad1ffba7795e&w=1060",
     },
