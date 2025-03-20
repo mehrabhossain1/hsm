349673374e9c5beaf3d1efc46b789d0e0bab8df6
Mehrab Hossain <mehrab.munna00@gmail.com>
2025-03-20 23:27:12 +0600

    blog

--- components/ui/BlogCard.tsx
+++ components/ui/BlogCard.tsx
@@ -81,7 +81,7 @@ export const BlogCard = ({
                                 damping: 17,
                             }}
                         >
-                            <span className=" font-bold">Read More</span>
+                            <span className=" font-bold">Explore</span>
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 width="24"
